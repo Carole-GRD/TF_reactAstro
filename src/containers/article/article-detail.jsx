068957b0c9a18f22c3b34c49fab235dcb3753741
Carole Gérard <gerard.carole@skynet.be>
2023-05-15 17:29:40 +0200

    end day

--- src/containers/article/article-detail.jsx
+++ src/containers/article/article-detail.jsx
@@ -17,29 +17,53 @@ const ArticleDetail = () => {
 
     const [details, setDetails] = useState([]);
     const [mark, setMark] = useState();
+    const [listStores, setListStores] = useState([]);
 
     // console.log('articleId : ', articleId);
+
 
     useEffect(() => {
         axios.get(`http://localhost:8080/api/article/${articleId}`)
             .then((article) => {
-                console.log('article : ', article);
                 setDetails(article.data.result);
-                return article.data.result.MarkId;
+
+                return {
+                    markId : article.data.result.MarkId,
+                    storesId : article.data.result.Stores.map(store => store.store_id)
+                }
+
             })
-            .then((markId) => {
-                axios.get(`http://localhost:8080/api/mark/${markId}`)
+            .then((data) => {
+                // les "data" fournies sont les données envoyées dans le "return" de la requête (voir ci-desssus)
+
+                axios.get(`http://localhost:8080/api/mark/${data.markId}`)
                     .then((mark) => {
-                        console.log('mark : ', mark);
                         setMark(mark.data.result.name)
+                    }) 
+                    .catch((error) => {
+                        console.error('Error fetching mark:', error);
+                    });   
+
+                axios.get('http://localhost:8080/api/store')
+                    .then((stores) => {
+                        console.log('stores : ', stores);
+                        
+                        // TODO : ↓ ici je récupère tous les stores mais il faut les filtrer pour ne récupérer que les magasins où l'on vend l'article en question
+                        // ATTENTION : ne faudrait-il pas le faire dans le composant article pour afficher le nom du magasin où l'article est vendu ?
+                        setListStores(stores.data.results);
                     })
-                    
+                    .catch((error) => {
+                        console.error('Error fetching stores:', error);
+                    });
             })
+            .catch((error) => {
+                console.error('Error fetching article:', error);
+            });
             
 
     }, []);
 
-    console.log('details : ', details);
+    // console.log('details : ', details);
 
 
     return (
@@ -63,6 +87,8 @@ const ArticleDetail = () => {
                     <img src={article} alt={`Image de ${details.name}`} />
 
                     <p>{details.description}</p>
+
+                    <p>Prix : ... €</p>
 
                 </section>
             </div>
