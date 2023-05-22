5d9d4d3bc730f96bcd208b95277e11220bbebd2d
Carole Gérard <gerard.carole@skynet.be>
2023-05-22 10:01:24 +0200

    fix store (redux persist)

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -28,31 +28,28 @@
 
 // configureStore.js
 
-import { configureStore } from '@reduxjs/toolkit';
-
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
- 
-
 import { combineReducers } from "redux";
-
 import authReducer from "./reducers/auth.reducer";
 import currentOrderReducer from './reducers/order.reducer';
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { persistStore, persistReducer } from 'redux-persist';
+import { configureStore } from '@reduxjs/toolkit';
+
 
 const rootReducer = combineReducers({
     auth: authReducer,
     order: currentOrderReducer
 });
- 
+
 const persistConfig = {
   key: 'root',
-  storage,
-  whitelist: ['auth', 'order'] // only navigation will be persisted
+  storage,                       // storage, <-> storage: storage,
+  whitelist: ['auth', 'order']   // only navigation will be persisted
 }
  
-
 const persistedReducer = persistReducer(persistConfig, rootReducer);
  
+
 export const store = configureStore({
     reducer: persistedReducer,
     devTools: import.meta.env.dev 
